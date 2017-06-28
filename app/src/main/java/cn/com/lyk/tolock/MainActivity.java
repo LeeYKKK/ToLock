85e8051953272027b4316edc635c6157520e1e99
LeeYKKK <282019754@qq.com>
2017-06-28 17:25:30 +0800

    去掉标题栏,添加图标

--- app/src/main/java/cn/com/lyk/tolock/MainActivity.java
+++ app/src/main/java/cn/com/lyk/tolock/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.com.lyk.tolock;
 
+import android.app.Activity;
 import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.Intent;
@@ -11,12 +12,13 @@ import android.view.Window;
 
 import cn.com.lyk.service.DetectorService1;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 1101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.activity_main);
         //检查如果我们运行在Android 5.0或更高版本
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
