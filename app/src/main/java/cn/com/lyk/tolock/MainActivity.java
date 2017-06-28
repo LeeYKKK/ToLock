551973345f3566131a745623be880ff85101fbd9
LeeYKKK <282019754@qq.com>
2017-06-28 15:30:14 +0800

    删除错误代码

--- app/src/main/java/cn/com/lyk/tolock/MainActivity.java
+++ app/src/main/java/cn/com/lyk/tolock/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //检查如果我们运行在Android 5.0或更高版本
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (!hasPermission()) {
